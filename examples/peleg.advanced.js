0aea1018033816efaa3462dbb30f0c9601d53b1a
Gabriel Moreira <gabriel@mobdiq.com.br>
2015-07-08 12:47:44 -0300

    Convert tabs to spaces

--- examples/peleg.advanced.js
+++ examples/peleg.advanced.js
@@ -1,5 +1,5 @@
 var peleg = require('..'),
-	Promise = require('bluebird');
+   Promise = require('bluebird');
 
 process.env.DEBUG='*';
 // Automatic log in/out messages
@@ -12,23 +12,23 @@ peleg.use(require('../lib/middleware/errorProcessor.js')(peleg));
 peleg.use(require('../lib/middleware/domainProcessor.js')());
 
 peleg.connect().then(function() {
-	peleg.queue("notification:send").publish({message: "Hello World!", date: new Date()});
-	peleg.queue("notification:send").publish({message: "Some error", date: new Date()});
-	peleg.queue("notification:send").publish({message: "Unhandled error", date: new Date()});
+   peleg.queue("notification:send").publish({message: "Hello World!", date: new Date()});
+   peleg.queue("notification:send").publish({message: "Some error", date: new Date()});
+   peleg.queue("notification:send").publish({message: "Unhandled error", date: new Date()});
 
-	peleg.queue("notification:send").process(function(data) {
-		var result = Promise.resolve();
-		// Success test case
-		console.log("The message", "'" + data.message + "' was sent at", new Date(data.date));
-		// Rejected test case
-		if (data.message === "Some error") return Promise.reject(data);
-		// Unhandled async error test case
-		if (data.message === "Unhandled error") {
-			setTimeout(function() {
-				throw new Error("Unhandled");
-			}, 20);
-			result = result.delay(200); // add some delay to mock async data...
-		}
-		return result;
-	})
+   peleg.queue("notification:send").process(function(data) {
+      var result = Promise.resolve();
+      // Success test case
+      console.log("The message", "'" + data.message + "' was sent at", new Date(data.date));
+      // Rejected test case
+      if (data.message === "Some error") return Promise.reject(data);
+      // Unhandled async error test case
+      if (data.message === "Unhandled error") {
+         setTimeout(function() {
+            throw new Error("Unhandled");
+         }, 20);
+         result = result.delay(200); // add some delay to mock async data...
+      }
+      return result;
+   })
 });
--- examples/peleg.basic.js
+++ examples/peleg.basic.js
@@ -1,11 +1,11 @@
 var peleg = require('..'),
-	Promise = require('bluebird');
+   Promise = require('bluebird');
 
 peleg.connect().then(function() {
-	peleg.queue("notification:send").publish({message: "Hello World!", date: new Date()});
+   peleg.queue("notification:send").publish({message: "Hello World!", date: new Date()});
 
-	peleg.queue("notification:send").process(function(data) {
-		console.log("The message", "'" + data.message + "' was sent at", new Date(data.date));
-		return Promise.resolve();
-	})
+   peleg.queue("notification:send").process(function(data) {
+      console.log("The message", "'" + data.message + "' was sent at", new Date(data.date));
+      return Promise.resolve();
+   })
 });
